f3446c9ce08a8e26dde9b7d4dc8fdfbadd1df385
aamnahmalik <aamnah@icloud.com>
2023-02-07 18:14:35 -0500

    created program to add two integers

--- Calculator.java
+++ Calculator.java
@@ -9,19 +9,5 @@ import java.util.Scanner;
 
 public class Calculator 
 {
-    static Scanner input = new Scanner(System.in);
-    public static void main(String[] args)
-    {
-        //ask for two numbers
-        System.out.println("Enter the first integer:");
-        int num1 = input.nextInt();
-        System.out.println("Enter the second integer:");
-        int num2 = input.nextInt();
-
-        //add numbers
-        int sum = num1 + num2;
-
-        //print sum
-        System.out.println(sum);
-    }
+    
 }
