6abb2ff890f6007103c37bfc466bcfe9958217b7
aamnahmalik <aamnah@icloud.com>
2023-02-07 18:42:43 -0500

    added option for subtraction

--- Calculator.java
+++ Calculator.java
@@ -9,5 +9,28 @@ import java.util.Scanner;
 
 public class Calculator 
 {
-    
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        //ask for two numbers
+        System.out.println("Enter the first integer:");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second integer:");
+        int num2 = input.nextInt();
+
+        //ask user if they want to add or subtract
+        System.out.println("Enter the number corresponding to the operation you would like to perform: ");
+        System.out.print("1. Addition \n2. Subtraction \n");
+        int operation = input.nextInt();
+
+        //perform and print result of operation
+        if (operation == 1)
+        {
+            System.out.println(num1 + num2);
+        }
+        else
+        {
+            System.out.println(num1 - num2);
+        }
+    }
 }
