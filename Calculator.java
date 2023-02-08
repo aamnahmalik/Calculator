2e34b7a195a8ff8e0be87d8aa35d42cd88662ed9
aamnahmalik <aamnah@icloud.com>
2023-02-07 19:00:48 -0500

    added divide by 0 error

--- Calculator.java
+++ Calculator.java
@@ -38,7 +38,14 @@ public class Calculator
         }
         else 
         {
-            System.out.println(num1/num2);
+            if (num2 == 0)
+            {
+                System.out.println("Error: Divide by Zero");
+            }
+            else
+            {
+                System.out.println(num1/num2);
+            }
         }
     }
 }
